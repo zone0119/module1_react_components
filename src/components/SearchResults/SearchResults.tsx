79d6815e6522ffc8255cb08d0bb9ba0327645a5b
zone0119 <54246376+zone0119@users.noreply.github.com>
2023-11-05 20:14:41 +0600

    add: pagination to SearchResults

--- src/components/SearchResults/SearchResults.tsx
+++ src/components/SearchResults/SearchResults.tsx
@@ -1,105 +1,105 @@
-import React, { useState, useEffect, FormEvent  } from 'react';
-import SearchForm from './SearchForm';
-import PokemonList from './PokemonList';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import paginationStyle from './Pagination.module.css';
 
 interface Pokemon {
   name: string;
   url: string;
 }
 
-interface SearchResultsProps {
-  setSearchResultProps: (result: unknown) => void;
-  handleSearch: (event: FormEvent<Element>) => void; 
-}
-
-
-const SearchResults: React.FC<SearchResultsProps> = ({ setSearchResultProps }) => {
+const SearchResults: React.FC = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
   const [filteredPokemons, setFilteredPokemons] = useState<Pokemon[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [count, setCount] = useState<number>(0);
+  const [paginationLinks, setPaginationLinks] = useState<JSX.Element[]>([]);
+  const navigate = useNavigate();
+  const { page } = useParams();
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
-      if (response.ok) {
-        const data = await response.json();
+  const resultsPerPage = 5;
+  const apiURL = `https://pokeapi.co/api/v2/pokemon`;
+
+  const loadPokemons = (pageNumber: number) => {
+    setIsLoading(true);
+    const offset = (pageNumber - 1) * resultsPerPage;
+    const url = `${apiURL}?offset=${offset}&limit=${resultsPerPage}`;
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed fetch data');
+        }
+        return response.json();
+      })
+      .then((data) => {
         setPokemons(data.results);
         setFilteredPokemons(data.results);
-        setIsLoading(true);
-      } else {
-        console.error('Failed to fetch data');
         setIsLoading(false);
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      setIsLoading(false);
-    } finally {
-      setTimeout(() => {
+
+        const numberOfPages = Math.ceil(data.count / resultsPerPage);
+
+        const currentPage = page ? parseInt(page, 10) : 1;
+        const links = [];
+        for (let i = 1; i <= numberOfPages; i++) {
+          links.push(
+            <Link
+            className={`${paginationStyle['divider']} ${i === currentPage ? paginationStyle['currentpage'] : ''}`}
+            key={i}
+            to={`/search/${i}`}
+            style={{ fontWeight: i === currentPage ? 'bold' : 'normal' }}
+          >
+            {i}
+          </Link>
+          );
+        }
+
+        setPaginationLinks(links);
+      })
+      .catch((error) => {
+        console.error('Error fetch data:', error);
         setIsLoading(false);
-      }, 1000);
-    }
+      });
   };
 
+  useEffect(() => {
+    loadPokemons(page ? parseInt(page, 10) : 1);
+  }, [page]);
 
   const handleSearch = (event: FormEvent<Element>) => {
-
     event.preventDefault();
     setIsLoading(true);
-  
-    const filtered = pokemons.filter(pokemon =>
+    const filtered = pokemons.filter((pokemon) =>
       pokemon.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
-  
-    setTimeout(() => {
-      setFilteredPokemons(filtered);
-      setIsLoading(false);
-      localStorage.setItem('searchTerm', searchQuery);
-    }, 5000);
+
+    setFilteredPokemons(filtered);
+    setIsLoading(false);
+    localStorage.setItem('searchTerm', searchQuery);
   };
-  
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newSearchQuery = event.target.value;
     setSearchQuery(newSearchQuery);
     localStorage.setItem('searchTerm', newSearchQuery);
   };
-  
-
-  const loadSearchTerm = () => {
-    const savedSearchTerm = localStorage.getItem('searchTerm');
-    if (savedSearchTerm && savedSearchTerm !== searchQuery) {
-      setSearchQuery(savedSearchTerm);
-    }
-  };
-  
-  useEffect(() => {
-    fetchData();
-    loadSearchTerm();
-  }, []);
-  
-  
-  
-
-  useEffect(() => {
-    fetchData();
-    loadSearchTerm();
-  }, []);  
-
-  console.log(typeof JSON.stringify(filteredPokemons[0]));
 
   return (
     <div>
-      <button onClick={() => setSearchResultProps(JSON.stringify(filteredPokemons[0]))}>+</button>
-
       <h1>Pokemon Search</h1>
-      <SearchForm
-        searchQuery={searchQuery}
-        handleSearch={handleSearch}
-        handleInputChange={handleInputChange}
-      />
-      {isLoading ? <div>Loading...</div> : <PokemonList filteredPokemons={filteredPokemons} />}
+      <form onSubmit={handleSearch}>
+        <input type="text" value={searchQuery} onChange={handleInputChange} />
+        <button type="submit">Search</button>
+      </form>
+      {isLoading ? <div>Loading...</div> : (
+        <ul>
+          {filteredPokemons.map((pokemon, index) => (
+            <li key={index}>{pokemon.name}</li>
+          ))}
+        </ul>
+      )}
+      <div className={paginationStyle['pagination-box']} >
+        {paginationLinks}
+      </div>
     </div>
   );
 };
