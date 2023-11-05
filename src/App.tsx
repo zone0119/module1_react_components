072e56cd67b18b5e4eab18911a408cce56984277
zone0119 <54246376+zone0119@users.noreply.github.com>
2023-11-05 20:09:58 +0600

    add: Route to APP

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-
-import './App.css'
-import Pages from './components/Pages/index'
-
-
+import React from 'react';
+import './App.css';
+import Pages from './components/Pages/index';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
 
+
+
+  const searchResult = 'search result..';
+
   return (
-    <>
-     <Pages />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Pages  searchResult={searchResult} />} />
+        <Route path="/search/:page" element={<Pages searchResult={searchResult} />} />
+        <Route path="/details/:itemId" element={<Pages  searchResult={searchResult} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
